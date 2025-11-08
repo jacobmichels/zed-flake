c969ce34a95b1a495ee7c97d8fc8ad8803c65000
jacobmichels <jacob.michels2025@gmail.com>
2025-11-08 12:44:26 -0500

    wip

--- flake.nix
+++ flake.nix
@@ -1,19 +1,19 @@
 {
-  description = "An empty flake template that you can adapt to your own environment";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # ID: 260395087
+    # URL: https://github.com/zed-industries/zed/releases/tag/v0.211.6
+    zed.url = "github:zed-industries/zed/394f56da42c9baa27f53714c486bccf5d95a8e3d";
+  };
 
-  # Flake inputs
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0"; # Stable Nixpkgs (use 0.1 for unstable)
-
-  # Flake outputs
   outputs =
     { self, ... }@inputs:
     let
-      # The systems supported for this flake's outputs
       supportedSystems = [
-        "x86_64-linux" # 64-bit Intel/AMD Linux
-        "aarch64-linux" # 64-bit ARM Linux
-        "x86_64-darwin" # 64-bit Intel macOS
-        "aarch64-darwin" # 64-bit ARM macOS
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
       ];
 
       # Helper for providing system-specific attributes
@@ -23,18 +23,14 @@
           system:
           f {
             inherit system;
-            # Provides a system-specific, configured Nixpkgs
             pkgs = import inputs.nixpkgs {
               inherit system;
-              # Enable using unfree packages
               config.allowUnfree = true;
             };
           }
         );
     in
     {
-      # Development environments output by this flake
-
       # To activate the default environment:
       # nix develop
       # Or if you use direnv:
@@ -42,22 +38,12 @@
       devShells = forEachSupportedSystem (
         { pkgs, system }:
         {
-          # Run `nix develop` to activate this environment or `direnv allow` if you have direnv installed
           default = pkgs.mkShellNoCC {
-            # The Nix packages provided in the environment
             packages = with pkgs; [
-              # Add the flake's formatter to your project's environment
               self.formatter.${system}
 
-              # Other packages
-              ponysay
+              nushell
             ];
-
-            # Set any environment variables for your development environment
-            env = { };
-
-            # Add any shell logic you want executed when the environment is activated
-            shellHook = "";
           };
         }
       );
---
+++ update.nu
@@ -0,0 +1,5 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i nu -p nushell
+#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/206fd45dccf38be08fbcf4cf3662db15a4f89139.tar.gz
+
+echo "Hello, World!"
